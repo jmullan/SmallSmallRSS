4d97e3afe0437fb42712f7d077caef0ad1a28509
jmullan <jmullan@visi.com>
2013-09-17 10:54:57 -0700

    argh fix sfbg hooks

--- plugins/af_sfbg/init.php
+++ plugins/af_sfbg/init.php
@@ -13,23 +13,30 @@ class Af_SFBG extends Plugin {
 
     function init($host) {
         $this->host = $host;
+        $host->add_hook($host::HOOK_GUID_FILTER, $this);
         $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
     }
 
     private static function cleanLink($link) {
         $parts = parse_url($link);
-        if (!isset($parts['path'])) {
+        if (!isset($parts['path']) || $parts['path'] == '') {
             $parts['path'] = '/';
         }
         return 'http://www.sfbg.com' . $parts['path'];
     }
 
     function hook_guid_filter($item, $guid) {
-        return self::cleanlink($item->get_link());
+        $link = $item->get_link();
+        if (FALSE === strpos($link, 'sfbg.com')) {
+            return $guid;
+        } else {
+            return self::cleanlink($link);
+        }
     }
 
     function hook_article_filter($article) {
-        if (FALSE === strpos($article['link'], 'sfbg.com')) {
+        $link = $article['link'];
+        if (FALSE === strpos($link, 'sfbg.com')) {
             return $article;
         }
         $article['link'] = self::cleanlink($link);

9e9caba1920aeb2aa0687726ceb4971caf87b0ea
Jesse Mullan <jmullan@visi.com>
2013-11-12 12:21:46 -0800

    add missing files

---
+++ SmallSmallRSS/Renderers/Insane.php
@@ -0,0 +1,16 @@
+<?php
+namespace SmallSmallRSS\Renderers;
+
+class Insance extends \SmallSmallRSS\Renderers\Base
+{
+    public function __construct($errors) {
+        $this->errors = $errors;
+    }
+    public function render()
+    {
+        $params = array('errors' => $this->errors);
+        header('Cache-Control: public');
+        $this->renderPHP(__DIR__ . '/templates/insane.php', $params);
+        exit;
+    }
+}
---
+++ SmallSmallRSS/Renderers/templates/insane.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <title>Startup failed</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" type="text/css" href="css/utility.css">
+</head>
+<body>
+  <div class="floatingLogo"><img src="images/logo_small.png"></div>
+  <div class="content">
+  <h1>Startup failed</h1>
+  <p>
+     Tiny Tiny RSS was unable to start properly. This usually means a misconfiguration or an incomplete upgrade.
+     Please fix errors indicated by the following messages:
+  </p>
+<?php
+foreach ($errors as $error) {
+     \SmallSmallRSS\Renderers\Messages::renderError($error);
+}
+?>
+  <p>
+    You might want to check tt-rss <a href="http://tt-rss.org/wiki">wiki</a>
+    or the <a href="http://tt-rss.org/forum">forums</a> for more information.
+    Please search the forums before creating new topic for your question.
+  </p>
+</div>
+</body>
+</html>

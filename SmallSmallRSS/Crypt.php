3b06338e8eca125a79095787861d297d3e612359
jmullan <jmullan@visi.com>
2013-11-11 20:49:18 -0800

    fix phpcs

--- SmallSmallRSS/Crypt.php
+++ SmallSmallRSS/Crypt.php
@@ -1,40 +1,31 @@
 <?php
 namespace SmallSmallRSS;
 
-class Crypt {
-
-    public static function de($str) {
+class Crypt
+{
+    public static function de($str)
+    {
         $key = hash('SHA256', FEED_CRYPT_KEY, true);
         $pair = explode(":", $str);
         if (count($pair) == 2) {
             @$iv = base64_decode($pair[0]);
             @$encstr = base64_decode($pair[1]);
             if ($iv && $encstr) {
-                $str = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $encstr,
-                                      MCRYPT_MODE_CBC, $iv);
+                $str = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $encstr, MCRYPT_MODE_CBC, $iv);
                 if ($str) {
                     return rtrim($str);
                 }
             }
         }
-
         return false;
     }
-    public static function en($str) {
+    public static function en($str)
+    {
         $key = hash('SHA256', FEED_CRYPT_KEY, true);
-
-        $iv = mcrypt_create_iv(
-            mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),
-            MCRYPT_RAND
-        );
-
-        $encstr = mcrypt_encrypt(
-            MCRYPT_RIJNDAEL_128, $key, $str,
-            MCRYPT_MODE_CBC, $iv);
-
+        $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND);
+        $encstr = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $str, MCRYPT_MODE_CBC, $iv);
         $iv_base64 = base64_encode($iv);
         $encstr_base64 = base64_encode($encstr);
-
         return "$iv_base64:$encstr_base64";
     }
 }

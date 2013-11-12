5669068e74f2466b0b8c5ab56a7e2a7f6941762c
Jesse Mullan <jmullan@visi.com>
2013-11-11 20:03:11 -0800

    fix psr2 errors for SmallSmallRSS/Utils.php and bootstrap

--- SmallSmallRSS/bootstrap.php
+++ SmallSmallRSS/bootstrap.php
@@ -1,42 +1,18 @@
 <?php
+/**
+ * Set up an application to use SmallSmallRSS
+ */
 
-function stripslashes_deep($value) {
-    $value = (
-        is_array($value)
-        ? array_map('stripslashes_deep', $value)
-        : stripslashes($value));
-    return $value;
-}
-
-function clean_up_stripslashes() {
-    static $finished = false;
-    if (!$finished) {
-        if (get_magic_quotes_gpc()) {
-            $_POST = array_map('stripslashes_deep', $_POST);
-            $_GET = array_map('stripslashes_deep', $_GET);
-            $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-            $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-        }
-        $finished = true;
-    }
-}
-
-function tiny_tiny_autoload($class) {
-    $class_file = str_replace("_", "/", strtolower(basename($class)));
-    $file = __DIR__ . "/../classes/$class_file.php";
-    if (file_exists($file)) {
-        require $file;
-    }
-}
+require_once __DIR__ . '/Utils.php';
 error_reporting(-1);
-
-clean_up_stripslashes();
-
-spl_autoload_register('tiny_tiny_autoload');
-
+Utils::cleanUpStripslashes();
+spl_autoload_register(array('Utils', 'smallSmallAutoload'));
 require __DIR__ . '/../jwage/SplClassLoader.php';
 $myLibLoader = new SplClassLoader('SmallSmallRSS', dirname(__DIR__));
 $myLibLoader->register();
-
-set_include_path(dirname(__DIR__) . '/include/' . PATH_SEPARATOR . get_include_path());
+$include_paths = array(
+    dirname(__DIR__) . '/include/',
+    get_include_path(),
+);
+set_include_path(join(PATH_SEPARATOR), $include_paths);
 require 'functions.php';

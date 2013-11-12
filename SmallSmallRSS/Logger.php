26b81a902c89d1f6a21b43f469c6df7ff623c55e
Jesse Mullan <jmullan@visi.com>
2013-11-12 09:54:51 -0800

    phpcbf SmallSmallRSS/Logger.php

--- SmallSmallRSS/Logger.php
+++ SmallSmallRSS/Logger.php
@@ -1,7 +1,8 @@
 <?php
 namespace SmallSmallRSS;
 
-class Logger {
+class Logger
+{
     private static $instance;
     private $adapter;
 
@@ -26,20 +27,25 @@ class Logger {
 
     public static $level = -1;
 
-    public static function log_error($errno, $errstr, $file, $line, $context) {
+    public static function log_error($errno, $errstr, $file, $line, $context)
+    {
         if (!$errno & self::$level) {
             return false;
         }
         return self::get()->adapter->log_error($errno, $errstr, $file, $line, $context);
     }
 
-    public static function log($string) {
+    public static function log($string)
+    {
         return self::get()->adapter->log($string);
     }
 
-    private function __clone() {}
+    private function __clone()
+    {
+    }
 
-    function __construct() {
+    function __construct()
+    {
         switch (LOG_DESTINATION) {
             case "sql":
                 $this->adapter = new Logger_SQL();
@@ -52,7 +58,8 @@ class Logger {
         }
     }
 
-    public static function get() {
+    public static function get()
+    {
         if (self::$instance == null) {
             self::$instance = new self();
         }

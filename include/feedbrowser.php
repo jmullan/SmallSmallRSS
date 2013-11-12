e4dc0ae3c2f5eb4a93c8527a010cf9dcba8f058b
Jesse Mullan <jmullan@visi.com>
2013-11-12 10:06:23 -0800

    beautify include/feedbrowser.php, reformat SQL in same

--- include/feedbrowser.php
+++ include/feedbrowser.php
@@ -1,44 +1,56 @@
 <?php
-function make_feed_browser($search, $limit, $mode = 1) {
+function make_feed_browser($search, $limit, $mode = 1)
+{
 
     $owner_uid = $_SESSION["uid"];
     $rv = '';
 
     if ($search) {
         $search_qpart = "AND (UPPER(feed_url) LIKE UPPER('%$search%') OR
-						UPPER(title) LIKE UPPER('%$search%'))";
+                        UPPER(title) LIKE UPPER('%$search%'))";
     } else {
         $search_qpart = "";
     }
 
     if ($mode == 1) {
-        /* $result = db_query("SELECT feed_url, subscribers FROM
-           ttrss_feedbrowser_cache WHERE (SELECT COUNT(id) = 0 FROM ttrss_feeds AS tf
-           WHERE tf.feed_url = ttrss_feedbrowser_cache.feed_url
-           AND owner_uid = '$owner_uid') $search_qpart
-           ORDER BY subscribers DESC LIMIT $limit"); */
-
-        $result = db_query("SELECT feed_url, site_url, title, SUM(subscribers) AS subscribers FROM
-						(SELECT feed_url, site_url, title, subscribers FROM ttrss_feedbrowser_cache UNION ALL
-							SELECT feed_url, site_url, title, subscribers FROM ttrss_linked_feeds) AS qqq
-						WHERE
-							(SELECT COUNT(id) = 0 FROM ttrss_feeds AS tf
-								WHERE tf.feed_url = qqq.feed_url
-									AND owner_uid = '$owner_uid') $search_qpart
-						GROUP BY feed_url, site_url, title ORDER BY subscribers DESC LIMIT $limit");
+        $result = db_query(
+            "SELECT feed_url, site_url, title, SUM(subscribers) AS subscribers
+             FROM (
+                 SELECT feed_url, site_url, title, subscribers FROM ttrss_feedbrowser_cache
+                 UNION ALL SELECT feed_url, site_url, title, subscribers FROM ttrss_linked_feeds
+             ) AS qqq
+             WHERE (
+                 SELECT COUNT(id) = 0 FROM ttrss_feeds AS tf
+                 WHERE
+                     tf.feed_url = qqq.feed_url
+                     AND owner_uid = '$owner_uid'
+             ) $search_qpart
+             GROUP BY feed_url, site_url, title ORDER BY subscribers DESC LIMIT $limit"
+        );
 
     } elseif ($mode == 2) {
-        $result = db_query("SELECT *,
-						(SELECT COUNT(*) FROM ttrss_user_entries WHERE
-					 		orig_feed_id = ttrss_archived_feeds.id) AS articles_archived
-						FROM
-							ttrss_archived_feeds
-						WHERE
-						(SELECT COUNT(*) FROM ttrss_feeds
-							WHERE ttrss_feeds.feed_url = ttrss_archived_feeds.feed_url AND
-								owner_uid = '$owner_uid') = 0	AND
-						owner_uid = '$owner_uid' $search_qpart
-						ORDER BY id DESC LIMIT $limit");
+        $result = db_query(
+            "SELECT
+                 *,
+                 (
+                     SELECT COUNT(*)
+                     FROM ttrss_user_entries
+                     WHERE
+                         orig_feed_id = ttrss_archived_feeds.id) AS articles_archived
+                     FROM
+                         ttrss_archived_feeds
+                     WHERE (
+                         SELECT COUNT(*)
+                         FROM ttrss_feeds
+                         WHERE
+                             ttrss_feeds.feed_url = ttrss_archived_feeds.feed_url
+                             AND owner_uid = '$owner_uid'
+                     ) = 0
+                     AND owner_uid = '$owner_uid'
+                     $search_qpart
+                     ORDER BY id DESC
+                     LIMIT $limit"
+        );
     }
 
     $feedctr = 0;
@@ -52,19 +64,19 @@ function make_feed_browser($search, $limit, $mode = 1) {
             $subscribers = $line["subscribers"];
 
             $check_box = "<input onclick='toggleSelectListRow2(this)'
-							dojoType=\"dijit.form.CheckBox\"
-							type=\"checkbox\" \">";
+                            dojoType=\"dijit.form.CheckBox\"
+                            type=\"checkbox\" \">";
 
             $class = ($feedctr % 2) ? "even" : "odd";
 
             $site_url = "<a target=\"_blank\"
-							href=\"$site_url\">
-							<span class=\"fb_feedTitle\">".
+                            href=\"$site_url\">
+                            <span class=\"fb_feedTitle\">".
                 htmlspecialchars($line["title"])."</span></a>";
 
             $feed_url = "<a target=\"_blank\" class=\"fb_feedUrl\"
-							href=\"$feed_url\"><img src='images/pub_set.svg'
-							style='vertical-align : middle'></a>";
+                            href=\"$feed_url\"><img src='images/pub_set.svg'
+                            style='vertical-align : middle'></a>";
 
             $rv .= "<li>$check_box $feed_url $site_url".
                 "&nbsp;<span class='subscribers'>($subscribers)</span></li>";
@@ -75,7 +87,7 @@ function make_feed_browser($search, $limit, $mode = 1) {
             $title = htmlspecialchars($line["title"]);
 
             $check_box = "<input onclick='toggleSelectListRow2(this)' dojoType=\"dijit.form.CheckBox\"
-							type=\"checkbox\">";
+                            type=\"checkbox\">";
 
             $class = ($feedctr % 2) ? "even" : "odd";
 
@@ -87,13 +99,13 @@ function make_feed_browser($search, $limit, $mode = 1) {
             }
 
             $site_url = "<a target=\"_blank\"
-							href=\"$site_url\">
-							<span class=\"fb_feedTitle\">".
+                            href=\"$site_url\">
+                            <span class=\"fb_feedTitle\">".
                 htmlspecialchars($line["title"])."</span></a>";
 
             $feed_url = "<a target=\"_blank\" class=\"fb_feedUrl\"
-							href=\"$feed_url\"><img src='images/pub_set.svg'
-							style='vertical-align : middle'></a>";
+                            href=\"$feed_url\"><img src='images/pub_set.svg'
+                            style='vertical-align : middle'></a>";
 
 
             $rv .= "<li id=\"FBROW-".$line["id"]."\">".

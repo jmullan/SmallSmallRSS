27dceff27421da276c076215c4283f864030b373
Jesse Mullan <jmullan@visi.com>
2013-11-20 18:37:38 -0800

    fix schecks warnings

--- SmallSmallRSS/FileCache.php
+++ SmallSmallRSS/FileCache.php
@@ -5,27 +5,31 @@ class FileCache {
     private static $default_caches = array("simplepie", "images", "export", "upload");
 
     public static function clearExpired() {
-        if (!is_writable($cache_dir)) {
-            _debug("$cache_dir is not writable");
-            return;
-        }
         $num_deleted = 0;
         foreach (self::$default_caches as $subdir) {
+          $num_deleted += self::clearExpiredDirContents($subdir);
         }
         return $num_deleted;
     }
+
     public static function clearExpiredDirContents($subdir) {
         // TODO sanitize input
-        $cache_dir = \SmallSmallRSS\Config::get('CACHE_DIR') . "/$dir";
+        $cache_dir = \SmallSmallRSS\Config::get('CACHE_DIR') . "/$subdir";
+        if (!is_writable($cache_dir)) {
+            _debug("$cache_dir is not writable");
+            return 0;
+        }
         $cache_lifetime = \SmallSmallRSS\Config::get('CACHE_LIFETIME');
         $files = glob("$cache_dir/*");
+        $num_deleted = 0;
         if ($files) {
             foreach ($files as $file) {
                 if (time() - filemtime($file) > $cache_lifetime) {
                     unlink($file);
-                    ++$num_deleted;
+                    $num_deleted += 1;
                 }
             }
         }
+        return $num_deleted;
     }
 }

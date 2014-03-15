19f866e58e0984d40986c860a4e35c33ff760547
Jesse Mullan <jmullan@visi.com>
2014-03-15 15:02:02 -0700

    backport a js change

--- web/js/functions.js
+++ web/js/functions.js
@@ -33,9 +33,7 @@ function exception_error(location, e, ext_info) {
     if (!ext_info) ext_info = false;
     try {
         if (ext_info) {
-            if (ext_info.responseText) {
-                ext_info = ext_info.responseText;
-            }
+            ext_info = JSON.stringify(ext_info);
         }
         try {
             new Ajax.Request("backend.php", {
